6dafc6594d489b430baca8226eaa952fecc68ddb
Amrita Virdi <virdi.amrita09@gmail.com>
2015-05-28 21:01:00 -0700

    css and init function..

--- The Prison Breaker_Ass!/Scripts/lib/game.js
+++ The Prison Breaker_Ass!/Scripts/lib/game.js
@@ -1,1 +1,13 @@
-﻿
+﻿/// <reference path="impress.js" />
+
+//source file name: game.js
+//Author: Amrita Virdi
+//Last Modified by: Amrita Virdi
+//Date Modified: 28/05/2015
+//Initialize impress.js
+
+
+function init() {
+    impress().init();
+    console.log("Game is Running......!!!"); 
+}
